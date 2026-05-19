0b67c09b5cf5898528283a9d18036f88b68c4793
MahmudDesigns <mdesigns.letstalk@gmail.com>
2026-05-19 20:03:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: '/YOUR_REPOSITORY_NAME/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
